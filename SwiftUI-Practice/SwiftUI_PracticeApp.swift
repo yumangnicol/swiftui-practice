cec32b26460109d39b03f25a05d98eedac25c763
nicolluisyumang <yumangnicol@gmail.com>
2023-08-02 10:58:54 +0100

    Initial Commit

---
+++ SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by Nicol Luis Yumang on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
